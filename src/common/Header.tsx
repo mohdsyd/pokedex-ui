141af9a659ccdbe496dfa4e57c155c4b81edfe22
Syed Mohammed <smoham14@hawk.iit.edu>
2022-08-30 21:50:19 -0500

    Adding Header

---
+++ src/common/Header.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import logo from '../logo.png';
+import './Header.css'
+
+interface IProps {
+  name?: string;
+}
+
+const Header: React.FC<IProps> = (props: IProps) => (
+    <div className='header'>
+        <img src={logo} className="app-logo" alt="logo" />
+    </div>
+);
+
+Header.defaultProps = {
+  name: 'world',
+};
+
+export default Header;
