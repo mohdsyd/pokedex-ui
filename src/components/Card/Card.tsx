724db0a2a3cdfdd686f8568bfd40abac523f9fdd
mohdsyd <smoham14@hawk.iit.edu>
2022-08-31 11:27:10 -0500

    style changes to cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,7 +7,8 @@ interface IProps {
 }
 
 const Card: React.FC<IProps> = (props: IProps) => (
-  <div className='card'>
+  <div onClick={()=>{console.log('here')}}className='card'>
+    <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'></img>
      <h1> {props.name}</h1>
   </div>  
 );
